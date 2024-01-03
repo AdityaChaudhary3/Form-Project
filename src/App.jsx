5d912396f820f3605b03d695cc669cc369324d6b
AdityaChaudhary3 <adityac2356@gmail.com>
2024-01-03 22:11:22 +0530

    Add Project.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>This is the form.</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App
