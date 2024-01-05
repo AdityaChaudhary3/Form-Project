6f2d33c85c749cc898dd96c286b6e8f3ecff538c
AdityaChaudhary3 <adityac2356@gmail.com>
2024-01-06 02:15:24 +0530

    Fix some minor mistake and setup json server.

--- src/pages/SecondForm.jsx
+++ src/pages/SecondForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import FormSecond from '../components/SecondFormComponent'
 import { useMyContext } from '../Context/Mycontext'
+import { useNavigate } from 'react-router-dom';
 function SecondForm() {
   const { globalObject } = useMyContext();
+  const navigate = useNavigate();
   const numofchildren = globalObject[0][25]["How many members in the Familly ?"];
   console.log(globalObject[0]);
   console.log(numofchildren);
+
   return (
     <div>
       <h1 className='font-bold'>Second Form for Now.</h1>
@@ -15,6 +18,7 @@ function SecondForm() {
           <FormSecond key={index} />  
         </>
       ))}
+      <button onClick={() => navigate("result")} className='cursor-pointer'>Final Submit.</button>
     </div>
   )
 }
