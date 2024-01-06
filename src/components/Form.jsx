a29e0446a09e6e65798c7e52302835080460bba2
Asad Rana <asadlukman246@gmail.com>
2024-01-06 06:59:09 +0530

    --------

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,31 +14,31 @@ export default function Form() {
   } ;
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-gray-200 p-4 flex flex-col justify-center items-center"> 
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">1. What is the Name of Block</label>
       <input className="w-1/2 p-3  h-2/3" type="text" placeholder="What is Name of Block ?" {...register("1.What is Name of Block ?", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">2.What is Name of Gram Panchayat.</label>
       <input className="w-1/2 p-3"  type="text" placeholder="What is Name of Gram Panchayat." {...register("2.What is Name of Gram Panchayat.", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">3.What is the Name of the head of the Familly ?</label>
       <input className="w-1/2 p-3"  type="text" placeholder="What is the Name of the head of the Familly ?" {...register("3.What is the Name of the head of the Familly ?", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">4.What is the Familly ID ? (Ration Card ID)</label>
       <input className="w-1/2 p-3"  type="text" placeholder="What is the Familly ID ? (Ration Card ID)" {...register("4.What is the Familly ID ? (Ration Card ID)", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">6.How much is the Familly Income ? (In Lakhs)</label>
       <input className="w-1/2 p-3"  type="text" placeholder="How much is the Familly Income ? (In Lakhs)" {...register("6.How much is the Familly Income ? (In Lakhs)", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">7.What is the Address of the Familly ?"</label>
       <input className="w-1/2 p-3"  type="text" placeholder="What is the Address of the Familly ?" {...register("7.What is the Address of the Familly ?", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">8.What is the category of the Familly ? (SC/ST/OBC/General)"</label>
       <select className="w-1/2" {...register("8.What is the category of the Familly ? (SC/ST/OBC/General)", { required: true })}>
       <option value="sc">SC</option>
@@ -47,11 +47,11 @@ export default function Form() {
             <option value="general">General</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">9.How much land the Familly Owns ? (In Bigha)"</label>
       <input className="w-1/2 p-3"  type="number" placeholder="How much land the Familly Owns ? (In Bigha)" {...register("9.How much land the Familly Owns ? (In Bigha)", {required: true})} />
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">10.Type of House  Ownership ?</label>
       <select className="w-1/2" {...register("10.Type of House  Ownership ?", { required: true })}>
             <option value="Self owned">Self Owned</option>
@@ -59,7 +59,7 @@ export default function Form() {
             <option value="Unauthorized">Unauthorized</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">11.Religion of the family?</label>
       <select className="w-1/2" {...register("11.Religion of the family?", { required: true })}>
       <option value="Hinduism">Hinduism</option>
@@ -67,14 +67,14 @@ export default function Form() {
             <option value="Christianity">Christianity </option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">12.Do the Familly have MGNREGA Job Card ?</label>
       <select className="w-1/2" {...register("12.Do the Familly have MGNREGA Job Card", { required: true })}>
       <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">13.Type of house</label>
       <select className="w-1/2" {...register("13.Type of house ?", { required: true })}>
       <option value="Kaccha">Kaccha</option>
@@ -82,77 +82,77 @@ export default function Form() {
        <option value="Half Pucca">Half Pucca</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">14.Do the Familly have electricity connection </label>
       <select className="w-1/2" {...register("14.Do the Familly have electricity connection ?", { required: true })}>
       <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">15. Do the Familly have Gas Connection ?</label>
       <select className="w-1/2" {...register("15.Do the Familly have Gas Connection ?", { required: true })}>
       <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">16.Do the Familly benefited through Ujjwala Scheme ? ?</label>
       <select className="w-1/2" {...register("16.Do the Familly benefited through Ujjwala Scheme ?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">17.Do the Familly have Individual toilets ?</label>
       <select className="w-1/2" {...register("17.Do the Familly have Individual toilets ?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">18.Whether received Awas under PMAY/CMAY?</label>
       <select className="w-1/2" {...register("18.Whether received Awas under PMAY/CMAY?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">19.Familly have taken loan under any scheme ?</label>
       <select className="w-1/2" {...register("19.Familly have taken loan under any scheme?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">20.Does the Familly covered under Fasal Bima Yojana ?</label>
       <select className="w-1/2" {...register("20.Does the Familly covered under Fasal Bima Yojana?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">21.Familly got connection under Har Ghar Jal Jal Jeevan Mission ?</label>
       <select className="w-1/2" {...register("21.Familly got connection under Har Ghar Jal Jal Jeevan Mission?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">22.Whether Familly got benefitted from CM Vivah Yojana ?</label>
       <select className="w-1/2" {...register("22.Whether Familly got benefitted from CM Vivah Yojana ?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">23. Familly got connection under Har Ghar Jal ?</label>
       <select className="w-1/2" {...register("23.Familly got connection under Har Ghar Jal ?", { required: true })}>
        <option value="Yes">Yes</option>
        <option value="No">No</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">24. Main Occupation of the Familly ?</label>
       <select className="w-1/2" {...register("24.Familly got connection under Har Ghar Jal ?", { required: true })}>
        <option value="Yes">Government Job</option>
@@ -161,11 +161,11 @@ export default function Form() {
        <option value="No">Daily Wage</option>
       </select>
       </div>
-      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4 w-4/5 h-10 flex items-center justify-around">
+      <div className="bg-white w-full rounded-lg h-auto p-3 mb-4  flex items-center justify-around">
       <label className="w-1/2">25.How many members in the Familly ?</label>
       <input className="w-1/2 p-3"  {...register("25.How many members in the Familly ?", { required: true })} type="number"/>
       </div>
-      <input className="w-1/2 p-3  cursor-pointer " type="submit" />
+      <input className="w-1/2 p-3 rounded-lg  border border-black cursor-pointer " type="submit" />
       </form>
   );
 }
--- src/pages/SecondForm.jsx
+++ src/pages/SecondForm.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import FormSecond from '../components/SecondFormComponent'
 import { useMyContext } from '../Context/Mycontext'
+import { useNavigate } from 'react-router-dom';
 function SecondForm() {
   const { globalObject } = useMyContext();
   const numofchildren = globalObject[0][25]["How many members in the Familly ?"];
   console.log(globalObject[0]);
   console.log(numofchildren);
+  const {navigate}=useNavigate();
+  const handlefinalsubmit=()=>{
+       navigate('/result');
+  }
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <h1 className='font-bold'>Second Form for Now.</h1>
       {Array.from({ length: numofchildren }, (_, index) => (
         <>
@@ -15,6 +20,7 @@ function SecondForm() {
           <FormSecond key={index} />  
         </>
       ))}
+      <div onClick={handlefinalsubmit} className='border border-black rounded-lg cursor-pointer w-full p-4 mt-3 sm:w-2/3'>Final Submit</div>
     </div>
   )
 }
